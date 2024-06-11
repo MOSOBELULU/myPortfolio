33ec862b5e349e037a6d0e697c74aa899dc90a2f
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-06-11 19:02:09 +0200

    wip

--- vite-project/src/sections/Projects.jsx
+++ vite-project/src/sections/Projects.jsx
@@ -1,19 +1,35 @@
 import classes from './Projects.module.css'
 
 export default function Projects() {
+  const projectList = [
+    {
+      title: "Budget Pal",
+      imageUrl: "/images/budget-logo.png",
+      link: "https://ur-budget-pal.vercel.app/"
+    },
+    {
+      title: "Next.js Blog",
+      imageUrl: "/images/blog.png",
+      link: "https://blog-nextjs-project-csnb.vercel.app/"
+    },
+    {
+      title: "Podcast App",
+      imageUrl: "/images/podcastapp.png",
+      link: "https://github.com/MOSOBELULU/PodcastApp.git"
+    }
+  ];
+
   return (
-    <div>
+    <div className={classes.projectsSection} id="Projects">
       <h1 className={classes.heading}>My Projects</h1>
-      <a href="https://ur-budget-pal.vercel.app/">
-        <img src="/images/budget-logo.png" alt="" className={classes.img} />
-      </a>
-      <a href="https://blog-nextjs-project-csnb.vercel.app/">
-        <img src="/images/blog.png" alt=""  className={classes.img}/>
-      </a>
-      <a href="https://github.com/MOSOBELULU/PodcastApp.git">
-        <img src="/images/podcastapp.png" alt=""  className={classes.img}/>
-      </a>
-      
+      <div className={classes.projectsGrid}>
+        {projectList.map((project, index) => (
+          <a href={project.link} key={index} className={classes.projectCard} target="_blank" rel="noopener noreferrer">
+            <img src={project.imageUrl} alt={project.title} className={classes.projectImage} />
+            <div className={classes.projectTitle}>{project.title}</div>
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
