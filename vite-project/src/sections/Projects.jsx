ce658e7fc6376d89fb630d8ea5053c5df2671f7f
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-06-17 13:44:55 +0200

    wip

--- vite-project/src/sections/Projects.jsx
+++ vite-project/src/sections/Projects.jsx
@@ -5,7 +5,8 @@ export default function Projects() {
     {
       title: "Budget Pal",
       imageUrl: "/images/budget-logo.png",
-      link: "https://ur-budget-pal.vercel.app/"
+      link: "https://ur-budget-pal.vercel.app/",
+      description: ' This is a Budget app that allows users to create their monthly budget by calculating their total income and expenses and determining the balance for the month. '
     },
     {
       title: "Next.js Blog",
@@ -15,8 +16,15 @@ export default function Projects() {
     {
       title: "Podcast App",
       imageUrl: "/images/podcastapp.png",
-      link: "https://github.com/MOSOBELULU/PodcastApp.git"
-    }
+      link: "https://github.com/MOSOBELULU/PodcastApp.git",
+      description: "This platform is designed to revolutionize your podcast experience. Browse shows, enjoy episodes, and curate your favorites effortlessly!"
+    },
+    {
+      title: "Recipe App",
+      imageUrl: "/images/recipe app.png",
+      link: "cooking-devs.vercel.app/",
+      description: "Recipe App offers a seamless culinary experience, boasting features like recipe card display, filtering options, search functionality, favourites management, editing capabilities, theme toggling, pagination, and detailed recipe previews."
+        }
   ];
 
   return (
@@ -27,6 +35,7 @@ export default function Projects() {
           <a href={project.link} key={index} className={classes.projectCard} target="_blank" rel="noopener noreferrer">
             <img src={project.imageUrl} alt={project.title} className={classes.projectImage} />
             <div className={classes.projectTitle}>{project.title}</div>
+            <div className={classes.description}>{project.description}</div>
           </a>
         ))}
       </div>
