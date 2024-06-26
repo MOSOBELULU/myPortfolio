c64f0e87b06d2e945468951f2a2a27332e44f9d8
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2024-06-26 20:21:34 +0200

    wip

--- vite-project/src/sections/EducationSection.jsx
+++ vite-project/src/sections/EducationSection.jsx
@@ -1,24 +1,28 @@
 import { educationData } from "../Components/DummyData";
-import classes from './EducationSection.module.css'
+import classes from './EducationSection.module.css';
 
- function EducationSection() {
+function EducationSection() {
     return (
         <div className={classes.EducationSection}>
-          <h1 className={classes.EducationHeading}>Education</h1>
-          <ul className={classes.EducationList}>
-            {educationData.map((item, index) => (
-              <li key={index} className={classes.EducationItem}>
-                   {/* {item.ImageUrl && <img src={item.ImageUrl} alt={item.Institution} className={classes.img} />} */}
-                   <div>
-                <h3>{item.Institution}</h3>
-                <p className={classes.Qualification}>{item.Qualification}</p>
-                <p className={classes.Duration}>{item.Duration}</p>
-                   </div>
-              </li>
-            ))}
-          </ul>
+            <h1 className={classes.EducationHeading}>Education</h1>
+            <div className={classes.SectionContainer}>
+                <div className={classes.ImageContainer}>
+                    <img className={classes.img} src="/images/Screenshot_20240206_192022_com.gbox.android.jpg" alt="Education" />
+                </div>
+                <ul className={classes.EducationList}>
+                    {educationData.map((item, index) => (
+                        <li key={index} className={classes.EducationItem}>
+                            <div>
+                                <h3>{item.Institution}</h3>
+                                <p className={classes.Qualification}>{item.Qualification}</p>
+                                <p className={classes.Duration}>{item.Duration}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-      );
+    );
 }
 
-export default EducationSection
+export default EducationSection;
